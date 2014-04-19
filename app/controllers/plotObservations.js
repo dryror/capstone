c5459eddbbdc76c7f474ae178f0f08a20ce2424d
Rory Drysdale <rorydrysdale@gmail.com>
2014-04-19 10:20:59 -0700

    Added footer to plotObservation view to display total percent coverage

--- app/controllers/plotObservations.js
+++ app/controllers/plotObservations.js
@@ -3,6 +3,11 @@ function editBtn(){
 	alert('You Clicked the Edit Button');
 }
 
+//Place holder for done button
+function doneBtn(){
+	alert('You Clicked the Done Button');
+}
+
 //Place holder for add button
 function addBtn(){
 	//Navigation to addPlotObservatino
