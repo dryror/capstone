a1b30208301c5853618b2203434057662a254ac6
Rory Drysdale <rorydrysdale@gmail.com>
2014-04-20 16:36:09 -0700

    Modified transectModal to use a navigation controller. Got the transects tableView to update on the modal closing.

--- app/controllers/transects.js
+++ app/controllers/transects.js
@@ -2,71 +2,85 @@
 var args = arguments[0];
 $.tbl.siteID = args.siteID;
 
-//Open Database
-var db = Ti.Database.open('ltemaDB');
-
-//Query - Retrieve existing sites from database
-rows = db.execute('SELECT transect_id, transect_name, surveyor ' + 
-				'FROM transect ' + 
-				'WHERE site_id = '+ $.tbl.siteID); 
-
-//get requested data from each row in table
-var id_counter = 0;
-while (rows.isValidRow()) {
-	id_counter++;
-	var transectID = rows.fieldByName('transect_id');	
-	var transectName = rows.fieldByName('transect_name');
-	var surveyor = rows.fieldByName('surveyor');
-
-	//create a string to display from each entry
-	var transectDesc =  transectName + ' - ' + surveyor; 
+function populateTable() {
 	
-	//Create a new row
-		var newRow = Ti.UI.createTableViewRow({
-			title : transectDesc,
-			transectID : transectID
-		});
+	//Clear the table if there is anything in it
+	var rd = []; 
+	$.tbl.data = rd;
+	
+	//Open Database
+	var db = Ti.Database.open('ltemaDB');
+	
+	//Query - Retrieve existing sites from database
+	rows = db.execute('SELECT transect_id, transect_name, surveyor ' + 
+					'FROM transect ' + 
+					'WHERE site_id = '+ $.tbl.siteID); 
+	
+	//get requested data from each row in table
+	var id_counter = 0;
+	while (rows.isValidRow()) {
+		id_counter++;
+		var transectID = rows.fieldByName('transect_id');	
+		var transectName = rows.fieldByName('transect_name');
+		var surveyor = rows.fieldByName('surveyor');
+	
+		//create a string to display from each entry
+		var transectDesc =  transectName + ' - ' + surveyor; 
 		
-		//create and add info icon for the row
-		var infoButton = Ti.UI.createButton({
-			style : Titanium.UI.iPhone.SystemButton.INFO_DARK,
-			right : 10,
-			height : 48, //this is deciding the size of the rows at the moment
-			width : 48, 
-			id : id_counter
-		});
-		newRow.add(infoButton);
-		
-   		//Add row to the table view
-  		$.tbl.appendRow(newRow);
+		//Create a new row
+			var newRow = Ti.UI.createTableViewRow({
+				title : transectDesc,
+				transectID : transectID
+			});
+			
+			//create and add info icon for the row
+			var infoButton = Ti.UI.createButton({
+				style : Titanium.UI.iPhone.SystemButton.INFO_DARK,
+				right : 10,
+				height : 48, //this is deciding the size of the rows at the moment
+				width : 48, 
+				id : id_counter
+			});
+			newRow.add(infoButton);
+			
+	   		//Add row to the table view
+	  		$.tbl.appendRow(newRow);
+	
+		rows.next();
+	}
+	rows.close();
+	db.close();
 
-	rows.next();
+
+	$.tbl.addEventListener('click', function(e){
+		//info icon clicked, get modal
+		if(e.source.toString() == '[object TiUIButton]') {
+			var modal = Alloy.createController("transectsModal", {transectID:e.rowData.transectID}).getView();
+			modal.open({
+				modal : true,
+				modalTransitionStyle : Ti.UI.iPhone.MODAL_TRANSITION_STYLE_COVER_VERTICAL,
+				modalStyle : Ti.UI.iPhone.MODAL_PRESENTATION_FORMSHEET,
+				navBarHidden : false
+			});
+		//row clicked, get transect view
+		} else {
+			var plots = Alloy.createController("plots").getView();
+	    	var nav = Alloy.Globals.navMenu;
+	    	nav.openWindow(plots);
+		} 
+	});
 }
-rows.close();
-db.close();
 
 
-$.tbl.addEventListener('click', function(e){
-	//info icon clicked, get modal
-	if(e.source.toString() == '[object TiUIButton]') {
-		var modal = Alloy.createController("transectsModal", {transectID:e.rowData.transectID}).getView();
-		modal.open({
-			modal : true,
-			modalTransitionStyle : Ti.UI.iPhone.MODAL_TRANSITION_STYLE_COVER_VERTICAL,
-			modalStyle : Ti.UI.iPhone.MODAL_PRESENTATION_FORMSHEET,
-			navBarHidden : false
-		});
-	//row clicked, get transect view
-	} else {
-		var plots = Alloy.createController("plots").getView();
-    	var nav = Alloy.Globals.navMenu;
-    	nav.openWindow(plots);
-	} 
+Ti.App.addEventListener("app:refreshTransects", function(e) {
+	populateTable();
 });
 
+/*
 Ti.App.addEventListener('updateTransects', function() {
 	alert('updateTransects called');
 });
+*/
 
 //Edit button toggle
 function editBtn(){
@@ -79,3 +93,5 @@ function addBtn(){
 	var nav = Alloy.Globals.navMenu;
 	nav.openWindow(addTransect);
 }
+
+populateTable();
--- app/controllers/transectsModal.js
+++ app/controllers/transectsModal.js
@@ -3,7 +3,7 @@ var args = arguments[0];
 var transectID = args.transectID;
 
 //Open database
-var db = Ti.Database.install('/ltema.sqlite', 'ltemaDB');
+var db = Ti.Database.open('ltemaDB');
 
 //Query - retrieve editable site values
 row = db.execute(	'SELECT transect_id, transect_name, surveyor, plot_distance, stake_orientation, comments ' +
@@ -32,14 +32,16 @@ $.surveyor.editable = false;
 $.comments.editable = false;
 
 //hide save button
-$.toggleSaveBtn.visible = false;    // in titanium getters and setters have not been implemented on very UI object.
+//$.toggleSaveBtn.visible = false;    // in titanium getters and setters have not been implemented on very UI object.
 									// this is one such case
 
 function backBtnClick(){
-	$.modalWin.close();
+	Ti.App.fireEvent("app:refreshTransects");
+	$.modalNav.close();
 }
 
 //swaps editable property of fields
+/*
 function toggleEdit(){
 	if ($.toggleEditSwitch.value == true) {
 		$.toggleLbl.visible = false;
@@ -61,10 +63,39 @@ function toggleEdit(){
 		alert("non-boolean toggleEditSwitch value");
 	};	
 }
+*/
+
+function editBtnClick(e){
+	//enable or disable edit mode
+    if (e.source.title == "Edit") {
+    	$.modalWin.editing = true;
+        e.source.title = "Done";
+        
+        //Enable editing
+        $.transectName.editable = true;
+		$.surveyor.editable = true;
+		$.comments.editable = true;
+        
+        //disable the button button during edit mode
+        $.backBtn.enabled = false;
+        
+    } else { 
+        $.modalWin.editing = false;
+        e.source.title = "Edit";
+        //enable the back button
+        $.backBtn.enabled = true;
+        
+        //disable editing
+        $.transectName.editable = false;
+		$.surveyor.editable = false;
+		$.comments.editable = false;
+		saveEdit();
+    }
+}
 
 function saveEdit(){
 	//Connect to database
-	var db = Ti.Database.install('/ltema.sqlite', 'ltemaDB');
+	var db = Ti.Database.open('ltemaDB');
 
 	//Query - update transect
 	row = db.execute(	'UPDATE OR FAIL transect ' + 
@@ -73,6 +104,6 @@ function saveEdit(){
 						', comments = \'' + $.comments.value + '\' ' +
 						'WHERE transect_id = ' + transectID);
 	db.close();
-	Ti.App.fireEvent('updateTransects');
-	$.modalWin.close();
+	//Ti.App.fireEvent('updateTransects');
+	//$.modalWin.close();
 }
