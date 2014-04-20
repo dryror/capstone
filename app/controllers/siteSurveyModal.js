68938e484579bc778914bca0543bd9e8c67142c0
Rory Drysdale <rorydrysdale@gmail.com>
2014-04-20 15:17:55 -0700

    Added biome to info screen

--- app/controllers/siteSurveyModal.js
+++ app/controllers/siteSurveyModal.js
@@ -1,17 +1,19 @@
 var args = arguments[0];
 var siteID = args.siteID;
 //Open database
-var db = Ti.Database.install('/ltema.sqlite', 'ltemaDB');
+var db = Ti.Database.open('ltemaDB');
 
 //Query - retrieve site values
-row = db.execute(	'SELECT site_id, year, protocol_name, park_name ' +
-					'FROM site_survey s, protocol p, park prk ' + 
-					'WHERE s.protocol_id = p.protocol_id ' + 
+row = db.execute(	'SELECT site_id, year, protocol_name, park_name, biome_name ' +
+					'FROM site_survey s, protocol p, park prk, biome bio ' + 
+					'WHERE p.biome_id = bio.biome_id ' +
+					'AND s.protocol_id = p.protocol_id ' + 
 					'AND s.park_id = prk.park_id ' +
 					'AND site_id = ' + siteID);
 					
 var siteYear = row.fieldByName('year');
 var protocolName = row.fieldByName('protocol_name');
+var biome = row.fieldByName('biome_name');
 var parkName = row.fieldByName('park_name');
 
 row.close();
@@ -22,6 +24,7 @@ $.parkName.text = parkName;
 $.siteID.text = siteID;
 $.siteYear.text = siteYear;
 $.protocolName.text = protocolName;
+$.biome.text = biome;
 
 function editBtnClick(){
 	alert("You clicked the edit button");
