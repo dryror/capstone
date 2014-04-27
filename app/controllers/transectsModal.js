cd0539548cb6c6d1fd9646208975d650b3c137f8
Nick Gulajec <nickg66@gmail.com>
2014-04-27 15:22:33 -0700

    Added tests to input fields

--- app/controllers/transectsModal.js
+++ app/controllers/transectsModal.js
@@ -12,8 +12,11 @@ row = db.execute(	'SELECT transect_id, transect_name, surveyor, plot_distance, s
 					'WHERE transect_id = ' + transectID);					
 
 var transectName = row.fieldByName('transect_name');
+var initialTransectName = transectName;
 var surveyor = row.fieldByName('surveyor');
+var initialSurveyor = surveyor;
 var plotDistance = row.fieldByName('plot_distance');
+var initialPlotDistance = plotDistance;
 var stakeOrientation = row.fieldByName('stake_orientation');
 var comments = row.fieldByName('comments');
 
@@ -102,8 +105,42 @@ function editBtnClick(e){
 
 //Save changes to transect
 function saveEdit(){
+	//validate input
+	//TODO: Confirm all conditions with project specs, project sponsor
+	var didError = false;
+	if ($.transectName.value.length < 2) {
+		alert('Transect Name should be at least 2 letters');
+		$.transectName.value = initialTransectName;
+		didError = true;
+	}
+	if ($.surveyor.value.length < 2) {
+		alert('Head surveyor name should be at least 2 letters');
+		$.surveyor.value = initialSurveyor;
+		didError = true;
+	}
+	if ($.plotDistance.value < 1) {
+		alert('Minimum plot distance is 1 meter');
+		$.plotDistance.value = initialPlotDistance;
+		didError = true;
+	}
+	if ($.plotDistance.value > 99) {
+		alert('Maximum plot distance is 99 meters');
+		$.plotDistance.value = initialPlotDistance;
+		didError = true;
+	}
+	
+	if (didError) {
+		return;
+	}
+	
+	//input is valid, store it
 	var db = Ti.Database.open('ltemaDB');
 	db.execute( 'UPDATE OR FAIL transect SET transect_name= ?, surveyor= ?, plot_distance= ?, comments= ? WHERE transect_id= ?',
 				$.transectName.value, $.surveyor.value, $.plotDistance.value, $.comments.value, transectID);
 	db.close();
+	
+	//refresh successfully stored values to cope with multiple edits
+	initialTransectName = $.transectName.value;
+	initialSurveyor = $.surveyor.value;
+	initialPlotDistance = $.plotDistance.value;
 }
