cbe41e6558e15c40b75e786b2641fe87ed8ffc43
kblairwhite <kblairwhite@gmail.com>
2014-05-02 16:53:08 -0700

    Name & Save photo to filesystem, Insert UTM info into transect table

--- app/controllers/addTransect.js
+++ app/controllers/addTransect.js
@@ -3,6 +3,12 @@ var args = arguments[0];
 var siteID = args.siteID;
 var transectCount = args.transectCount;
 
+// Initialize Variables
+var photo;
+var utmEasting;
+var utmNorthing;
+var utmZone;
+
 //validate form before inserting to database
 function doneBtn(){
 	if ($.tsctName.value == "") {
@@ -29,17 +35,36 @@ function doneBtn(){
 			stakeText = "Top Right / Bottom Left";
 		}
 
-		//Connect to database
-		var db = Ti.Database.open('ltemaDB');
-	
-		//Insert Query - add row to transect table
-		db.execute(	'INSERT INTO transect (transect_name,surveyor,other_surveyors,plot_distance,stake_orientation,comments,site_id) VALUES (?,?,?,?,?,?,?)', 
-					$.tsctName.value, $.srvyName.value, $.otherSrvyName.value, $.plotDist.value, stakeText, $.comments.value, siteID);
-		//Close the database
-		db.close();
-		//refresh and close
-		Ti.App.fireEvent("app:refreshTransects");
-		$.addTransect.close();
+		//** will need to clean up and add validation
+		//*** Put a if photo exists here****
+		var photoName = savePhoto(photo, transectCount);
+		
+		try{
+			//Connect to database
+			var db = Ti.Database.open('ltemaDB');
+			
+			//add photo name to media table
+			db.execute( 'INSERT INTO media (media_name) VALUES (?)', photoName);
+			
+			//get the id of the last row inserted into the database - *not sure if this is acceptable sql code to use?
+			var results = db.execute('SELECT last_insert_rowid() as mediaID');
+			var mediaID = results.fieldByName('mediaID');
+			
+			//Insert Query - add row to transect table
+			db.execute(	'INSERT INTO transect (transect_name,surveyor,other_surveyors,plot_distance,stake_orientation,utm_zone,utm_easting,utm_northing,comments,site_id,media_id) VALUES (?,?,?,?,?,?,?,?,?,?,?)', 
+						$.tsctName.value, $.srvyName.value, $.otherSrvyName.value, $.plotDist.value, stakeText, utmZone, utmEasting, utmNorthing, $.comments.value, siteID, mediaID);
+						
+		}catch(e){
+			
+		}finally{	
+			//close the result set
+			results.close();	
+			//Close the database
+			db.close();
+			//refresh and close
+			Ti.App.fireEvent("app:refreshTransects");
+			$.addTransect.close();
+		}
 	}
 }
 
@@ -51,7 +76,34 @@ function takePhoto() {
 		//Set thumbnail
 		$.transectThumbnail.image = myPhoto;
 		
-		alert("UTMEasting: " + UTMEasting + "\nUTMNorthing: " + UTMNorthing + "\nUTMZone: " + n_UTMZone);
-	
+		//set variables with values
+		photo = myPhoto;
+		utmEasting = UTMEasting;
+		utmNorthing = UTMNorthing;
+		utmZone = n_UTMZone;
+		
+		//alert("UTMEasting: " + UTMEasting + "\nUTMNorthing: " + UTMNorthing + "\nUTMZone: " + n_UTMZone);
 	});
+}
+
+//Name and save photo to filesystem - do this when done btn is pressed
+function savePhoto(photo, transectCount){
+	var dir = "site" + siteID;
+	var img = photo; 
+	var filename = "T"+ (transectCount + 1);
+	
+	// Create image Directory for site
+	var imageDir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, dir);
+	if (! imageDir.exists()) {
+    	imageDir.createDirectory();
+	}
+	
+	// .resolve() provides the resolved native path for the directory.
+	var imageFile  = Ti.Filesystem.getFile(imageDir.resolve(), filename + '.jpg');
+	imageFile.write(img);
+	
+	//var path = dir + Ti.Filesystem.separator + filename + '.jpg';
+	
+	var path = filename + '.jpg';
+	return path;
 }
