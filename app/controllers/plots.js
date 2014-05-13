7dd90fe258bbbd5e0c7443ef18acb236d4d3f7a0
Rory Drysdale <rorydrysdale@gmail.com>
2014-05-12 17:37:59 -0700

    Added code to plots screen to start work on addPlot

--- app/controllers/plots.js
+++ app/controllers/plots.js
@@ -1,3 +1,7 @@
+//get transectID from calling window
+var args = arguments[0];
+$.tbl.transectID = args.transectID; //Not sure this needs to be $.tbl.transectId. Copied from transects.js.
+
 //Place holder for edit button
 function editBtn(){
 	alert('You Clicked the Edit Button');
@@ -6,14 +10,22 @@ function editBtn(){
 //Place holder for add button
 function addBtn(){
 	//Navigation to addPlot
-	var addPlot = Alloy.createController("addPlot").getView();
+	var addPlot = Alloy.createController("addPlot", {transectID: $.tbl.transectID}).getView();
 	var nav = Alloy.Globals.navMenu;
 	nav.openWindow(addPlot);
 }
 
-//Will be replaced once controller implemented
-$.row1.addEventListener('click', function(e){
-    var observations = Alloy.createController("plotObservations", {plotID:1}).getView(); //hardcoded for testing, replace with {plotID:e.rowData.plotID} when wired
-    var nav = Alloy.Globals.navMenu;
-    nav.openWindow(observations);
-}); 
+function populateTable() {
+	//Will be replaced once controller implemented
+	$.row1.addEventListener('click', function(e){
+	    var observations = Alloy.createController("plotObservations", {plotID:1}).getView(); //hardcoded for testing, replace with {plotID:e.rowData.plotID} when wired
+	    var nav = Alloy.Globals.navMenu;
+	    nav.openWindow(observations);
+	}); 
+}
+
+populateTable();
+
+Ti.App.addEventListener("app:refreshPlots", function(e) {
+	populateTable();
+});
