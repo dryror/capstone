996f53cf11a4c86b73ad703a65a1772baeb9f9aa
kblairwhite <kblairwhite@gmail.com>
2014-05-15 10:46:58 -0700

    Retrieve existing plots from database and list

--- app/controllers/plots.js
+++ app/controllers/plots.js
@@ -16,16 +16,60 @@ function addBtn(){
 }
 
 function populateTable() {
-	//Will be replaced once controller implemented
-	$.row1.addEventListener('click', function(e){
-	    var observations = Alloy.createController("plotObservations", {plotID:9}).getView(); //hardcoded for testing, replace with {plotID:e.rowData.plotID} when wired
-	    var nav = Alloy.Globals.navMenu;
-	    nav.openWindow(observations);
-	}); 
+	//Clear the table if there is anything in it
+	var rd = []; 
+	$.tbl.data = rd;
+	try {
+		//open database
+		var db = Ti.Database.open('ltemaDB');
+		
+		//Query - Retrieve existing plots from database
+		var rows = db.execute('SELECT plot_id, plot_name \
+							FROM plot \
+							WHERE transect_id = ?', $.tbl.transectID);
+		
+		//Get requested data from each row in table
+		while (rows.isValidRow()) {	
+			var plotID = rows.fieldByName('plot_id');
+			var plotName = rows.fieldByName('plot_name');
+
+			//create a new row
+				var newRow = Ti.UI.createTableViewRow({
+					title : plotName,
+					plotID : plotID
+				});
+				
+				//create and add info icon for the row
+				var infoButton = Ti.UI.createButton({
+					style : Titanium.UI.iPhone.SystemButton.DISCLOSURE,
+					right : 10,
+					height : 48, //this is deciding the size of the rows at the moment
+					width : 48, 
+				});
+				newRow.add(infoButton);
+				
+		   		//Add row to the table view
+		  		$.tbl.appendRow(newRow);
+		
+			rows.next();
+		}
+	} catch(e){
+		Ti.App.fireEvent("app:dataBaseError", e);
+	} finally {
+		rows.close();
+		db.close();
+	}
 }
 
 populateTable();
 
 Ti.App.addEventListener("app:refreshPlots", function(e) {
 	populateTable();
+});
+
+//Plot TableView event listener
+$.tbl.addEventListener('click', function(e){
+	 var observations = Alloy.createController("plotObservations", {plotID:e.rowData.plotID}).getView();
+	 var nav = Alloy.Globals.navMenu;
+	 nav.openWindow(observations);   
 });
