802e10dc91e26ba769b4ec96917a236f61ceff0b
Rory Drysdale <rorydrysdale@gmail.com>
2014-05-21 16:12:48 -0700

    Changes to addPlot tests

--- app/assets/spec/ui.js
+++ app/assets/spec/ui.js
@@ -197,19 +197,27 @@ describe('\n\nThe addPlot screen of transectID 1', function() {
 	describe('\naddPlot form input', function() {
 		it('should set stake orientation', function() {
 			$.pickStake.setIndex(1);
-			expect($.pickStake.index == 1).toBe(true);
+			expect($.pickStake.index).toBe(1);
+			$.pickStake.setIndex(0);
+			expect($.pickStake.index).toBe(0);
 		});
-		it('should set stake orientation to Top and Bottome', function() {
+		it('should set stake deviation', function() {
 			$.stakeDeviation.setValue("Top and Bottom");
 			expect($.stakeDeviation.value).toBe("Top and Bottom");
+			$.stakeDeviation.setValue("Right and Left");
+			expect($.stakeDeviation.value).toBe("Right and Left");
 		});
 		it('should set plot distance', function() {
 			$.pickDistance.setIndex(0);
-			expect($.pickDistance.index == 0).toBe(true);
+			expect($.pickDistance.index).toBe(0);
+			$.pickDistance.setIndex(1);
+			expect($.pickDistance.index).toBe(1);
 		});
-		it('should set plot distance to 15', function() {
+		it('should set plot deviation', function() {
 			$.distanceDeviation.setValue(15);
 			expect($.distanceDeviation.value).toBe(15);
+			$.distanceDeviation.setValue(4);
+			expect($.distanceDeviation.value).toBe(4);
 		});
 	});
 });
