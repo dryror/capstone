1161cea63d02afb79e1b4a5d799a0296ad6f2a2d
kblairwhite <kblairwhite@gmail.com>
2014-05-21 17:32:13 -0700

    Updated Plot Screen error handling

--- app/controllers/plots.js
+++ app/controllers/plots.js
@@ -1,10 +1,98 @@
+/* List screen to view, add, or delete plots */
+
 //get transectID from calling window
 var args = arguments[0];
 var transectID = args.transectID;
 $.tbl.transectID = transectID;
 
-
 populateTable();
+toggleAddBtn();
+
+/* Nav Bar Label */
+
+// Build title label
+var parkName = "";
+var transectName ="";
+
+try {
+	var db = Ti.Database.open('ltemaDB');
+	
+	resultRow = db.execute (	'SELECT p.park_name, t.transect_name \
+								FROM park p, transect t, site_survey s \
+								WHERE s.site_id = t.site_id \
+								AND p.park_id = s.park_id \
+								AND t.transect_id = ?' , transectID);
+	parkName = resultRow.fieldByName('park_name');
+	transectName = resultRow.fieldByName('transect_name');
+} catch (e) {
+	var errorMessage = e.message;
+	Ti.App.fireEvent("app:dataBaseError", {error: errorMessage});
+} finally {
+	resultRow.close();
+	db.close();
+}
+
+var labelText = parkName + ' - ' + transectName;
+
+var titleLabel = Titanium.UI.createLabel({
+	top:10,
+	text: labelText,
+	textAlign:'center',
+	font:{fontSize:20,fontWeight:'bold'},
+});
+
+// Associate label to title
+$.plotsWin.setTitleControl(titleLabel);
+
+
+/* Event Listeners */
+
+Ti.App.addEventListener("app:refreshPlots", function(e) {
+	populateTable();
+	toggleAddBtn();
+});
+
+//Plot TableView - event listener
+$.tbl.addEventListener('click', function(e){
+	//info button clicked, display modal
+	if(e.source.toString() == '[object TiUIButton]') {
+		var modal = Alloy.createController("plotsModal", {plotID:e.rowData.plotID, title:e.rowData.title}).getView();
+		modal.open({
+			modal : true,
+			modalTransitionStyle : Ti.UI.iPhone.MODAL_TRANSITION_STYLE_COVER_VERTICAL,
+			modalStyle : Ti.UI.iPhone.MODAL_PRESENTATION_PAGESHEET,
+			navBarHidden : false
+		});
+	//row clicked, get transect view
+	}else{  
+		//open plot observations
+		var observations = Alloy.createController("plotObservations", {plotID:e.rowData.plotID}).getView();
+		var nav = Alloy.Globals.navMenu;
+		nav.openWindow(observations);   
+	}
+});
+
+//Delete - event listener
+$.tbl.addEventListener('delete', function(e) { 
+	//get the plot_id of the current row to be deleted
+	var currentPlotID = e.rowData.plotID;
+	try{
+		//open database
+		var db = Ti.Database.open('ltemaDB');
+		
+		//delete current row from the database
+		db.execute('DELETE FROM plot WHERE plot_id = ?', currentPlotID);
+	} catch(e) {
+		var errorMessage = e.message;
+		Ti.App.fireEvent("app:dataBaseError", {error: errorMessage});
+	} finally {
+		db.close();
+		toggleEditBtn();
+	}
+});
+
+
+/* Functions */
 
 function populateTable() {
 	//Clear the table if there is anything in it
@@ -56,100 +144,32 @@ function populateTable() {
 	}
 }
 
-/* Nav Bar Label */
-
-// Build title label
-var parkName = "";
-var transectName ="";
-
-try {
-	var db = Ti.Database.open('ltemaDB');
+//EDIT BUTTON - toggle edit mode
+function editBtn(e){
 	
-	resultRow = db.execute (	'SELECT p.park_name, t.transect_name \
-								FROM park p, transect t, site_survey s \
-								WHERE s.site_id = t.site_id \
-								AND p.park_id = s.park_id \
-								AND t.transect_id = ?' , transectID);
-	parkName = resultRow.fieldByName('park_name');
-	transectName = resultRow.fieldByName('transect_name');
-} catch (e) {
-	var errorMessage = e.message;
-	Ti.App.fireEvent("app:dataBaseError", {error: errorMessage});
-} finally {
-	resultRow.close();
-	db.close();
-}
-
-var labelText = parkName + ' - ' + transectName;
-
-var titleLabel = Titanium.UI.createLabel({
-	top:10,
-	text: labelText,
-	textAlign:'center',
-	font:{fontSize:20,fontWeight:'bold'},
-});
-
-// Associate label to title
-$.plotsWin.setTitleControl(titleLabel);
-
-
-/* Event Listeners */
-
-Ti.App.addEventListener("app:refreshPlots", function(e) {
-	populateTable();
-});
-
-//Plot TableView - event listener
-$.tbl.addEventListener('click', function(e){
-	//info button clicked, display modal
-	if(e.source.toString() == '[object TiUIButton]') {
-		var modal = Alloy.createController("plotsModal", {plotID:e.rowData.plotID, title:e.rowData.title}).getView();
-		modal.open({
-			modal : true,
-			modalTransitionStyle : Ti.UI.iPhone.MODAL_TRANSITION_STYLE_COVER_VERTICAL,
-			modalStyle : Ti.UI.iPhone.MODAL_PRESENTATION_PAGESHEET,
-			navBarHidden : false
-		});
-	//row clicked, get transect view
-	}else{  
-		//open plot observations
-		var observations = Alloy.createController("plotObservations", {plotID:e.rowData.plotID}).getView();
-		var nav = Alloy.Globals.navMenu;
-		nav.openWindow(observations);   
-	}
-});
-
-//Delete - event listener
-$.tbl.addEventListener('delete', function(e) { 
-	//get the plot_id of the current row to be deleted
-	var currentPlotID = e.rowData.plotID;
-	try{
-		//open database
-		var db = Ti.Database.open('ltemaDB');
+	//enable or disable edit mode
+	if (e.source.title == "Edit") {
+		$.tbl.editing = true;
+		e.source.title = "Done";
+		//disable the add button during edit mode
+		$.addPlot.enabled = false;
 		
-		//delete current row from the database
-		db.execute('DELETE FROM plot WHERE plot_id = ?', currentPlotID);
-	} catch(e) {
-		var errorMessage = e.message;
-		Ti.App.fireEvent("app:dataBaseError", {error: errorMessage});
-	} finally {
-		db.close();
+	} else { 
+		$.tbl.editing = false;
+		e.source.title = "Edit";
+		toggleAddBtn();
 	}
-	
-	//check if Edit button should be enabled/disabled - if no rows exist
-	toggleEditBtn();
-});
-
-
-/* Functions */
-
-//Place holder for edit button
-function editBtn(){
-	alert('You Clicked the Edit Button');
 }
 
 //ADD BUTTON - add a new plot
 function addBtn(){	
+	//Navigation to addPlot
+	var addPlot = Alloy.createController("addPlot", {transectID: $.tbl.transectID}).getView();
+	var nav = Alloy.Globals.navMenu;
+	nav.openWindow(addPlot);
+}		
+
+function toggleAddBtn(){
 	//check if any rows exists
 	if(showTotalRowNumber() > 0){
 		
@@ -159,15 +179,25 @@ function addBtn(){
 		
 			//check the total ground cover of last plot entry
 			if(totalGroundCover < 100){
-				alert("Total Ground Cover is less than 100% \n" + lastPlotEntryName);
-				return;
+				//show error message
+				var addErrorMessage = "* " + lastPlotEntryName + "'s " + "Total Ground Coverage is less than 100%";
+				$.addPlotError.text = addErrorMessage;
+				$.addPlotError.visible = true;
+				
+				//disable add plot button
+				$.addPlot.enabled = false;
+				
+				//set the last plot entry to red text
+				var lastRow = showTotalRowNumber() - 1;
+				$.tbl.data[0].rows[lastRow].color = "red";
+			
+			}else{
+				//if the total ground cover is 100% or greater
+				$.addPlot.enabled = true;
+				$.addPlotError.visible = false;
 			}
-		}
-			//Navigation to addPlot
-			var addPlot = Alloy.createController("addPlot", {transectID: $.tbl.transectID}).getView();
-			var nav = Alloy.Globals.navMenu;
-			nav.openWindow(addPlot);
-}		
+	}
+}
 
 // get the total ground cover of the last plot entry
 function getTotalGroundCover(){
@@ -207,13 +237,14 @@ function toggleEditBtn(){
 	//if no rows exist
 	if(numRows <= 0){
 		//disable Edit Button
-		$.editSite.enabled = false;
-		$.editSite.title = "Edit";
-		$.addSite.enabled = true;
+		$.editPlot.enabled = false;
+		$.editPlot.title = "Edit";
+		$.addPlot.enabled = true;
 		$.tbl.editing = false;
+		$.addPlotError.visible = false;
 	}else{
 		//enable Edit Button
-		$.editSite.enabled = true;
+		$.editPlot.enabled = true;
 	}
 }
 
@@ -232,20 +263,3 @@ function showTotalRowNumber(){
 	return totalRows;
 }
 
-//Edit button toggle
-function editBtn(e){
-	
-	//enable or disable edit mode
-	if (e.source.title == "Edit") {
-		$.tbl.editing = true;
-		e.source.title = "Done";
-		//disable the add and export buttons during edit mode
-		$.addSite.enabled = false;
-		
-	} else { 
-		$.tbl.editing = false;
-		e.source.title = "Edit";
-		//enable the add and export button
-		$.addSite.enabled = true;
-	}
-}
