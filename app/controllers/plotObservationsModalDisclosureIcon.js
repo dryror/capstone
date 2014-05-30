1e78dbc4c895b64faca20e68665aae7b5891bad2
kblairwhite <kblairwhite@gmail.com>
2014-05-29 18:05:22 -0700

    Added Preview thumbnail to plotObservationDisclosureModal

--- app/controllers/plotObservationsModalDisclosureIcon.js
+++ app/controllers/plotObservationsModalDisclosureIcon.js
@@ -9,6 +9,7 @@ var observationID = args.observationID;
 var title = args.title;
 var comments = args.comments;
 var mediaID = args.mediaID;
+var siteID = args.siteID;
 
 //Query the database, assign returned ground cover to TextField
 try {
@@ -17,7 +18,52 @@ try {
 						FROM plot_observation \
 						WHERE observation_id = ?', observationID);
 	var groundCover = resultRow.fieldByName('ground_cover');
+	//var mediaID = resultRow.fieldByName('media_id');
+	
 	$.groundCover.value = groundCover;
+	
+		//if media does not exist
+	if(mediaID != null){
+
+	//get the media name
+	var mediaRow = db.execute('SELECT media_name \
+							FROM media \
+							WHERE media_id = ?', mediaID);
+	
+	var mediaName = mediaRow.fieldByName('media_name');	
+	
+	//GET FOLDER NAME - Retrieve site survery, year, park
+	var rows = db.execute('SELECT year, protocol_name, park_name \
+							FROM site_survey s, protocol p, park prk \
+							WHERE s.protocol_id = p.protocol_id \
+							AND s.park_id = prk.park_id \
+							AND site_id = ?', siteID);
+							
+   //get the name of the directory	
+	var year = rows.fieldByName('year');
+	var protocolName = rows.fieldByName('protocol_name');
+	var parkName = rows.fieldByName('park_name');
+
+	var folderName = year + ' - ' + protocolName + ' - ' + parkName;
+	
+	var imageDir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, folderName);
+	
+	if (imageDir.exists()) {		
+		// .resolve() provides the resolved native path for the directory.
+		var imageFile = Ti.Filesystem.getFile(imageDir.resolve(), mediaName);
+		if (imageFile.exists()) {
+			//Set thumbnail
+			$.plotObservationThumbnail.visible = true;
+			$.plotObservationThumbnail.image = imageFile;
+		
+			//Save Photo for preview (temporary photo)
+			var temp = Ti.Filesystem.getFile(Titanium.Filesystem.tempDirectory,'temp.png');
+			temp.write(imageFile);
+		}
+	}
+ }	
+	
+	
 } catch (e) {
 	var errorMessage = e.message;
 	Ti.App.fireEvent("app:dataBaseError", {error: errorMessage});
@@ -46,12 +92,32 @@ $.comments.text = comments;
 
 // Valid inputs get saved to the database and this screen closes.
 function doneBtnClick(){
+	//remove the temp photo - used for photo preview 
+	var tempPhoto = Ti.Filesystem.getFile(Titanium.Filesystem.tempDirectory,'temp.png');
+	if(tempPhoto.exists){
+		tempPhoto.deleteFile();
+	}
 	$.modalNav.close();
 	Ti.App.fireEvent("app:refreshPlotObservations");
 }
 
 // Exit this screen without saving
 function cancelBtnClick(){
+	//remove the temp photo - used for photo preview 
+	var tempPhoto = Ti.Filesystem.getFile(Titanium.Filesystem.tempDirectory,'temp.png');
+	if(tempPhoto.exists){
+		tempPhoto.deleteFile();
+	}
 	$.modalNav.close();
 }
 
+//THUMBNAIL BUTTON - preview photo
+function previewPhoto(){
+	var modal = Alloy.createController("photoPreviewModal", {}).getView();
+		modal.open({
+			modal : true,
+			modalTransitionStyle : Ti.UI.iPhone.MODAL_TRANSITION_STYLE_COVER_VERTICAL,
+			modalStyle : Ti.UI.iPhone.MODAL_PRESENTATION_PAGESHEET,
+			navBarHidden : false
+	});
+}
